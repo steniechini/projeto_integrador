e78ed71b0e2da07e9362e1601d06e8e1f8e52ccb
stephanie <stephaniechini00@gmail.com>
2023-06-12 22:24:41 -0300

    1206

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
     <div class=" col-12">
         <footer>
             <ul>
-                <li><a href="https://twitter.com" target="_blank"><img src="img/logotwitter.png" alt="logo twitter"></a></li>
-                <li><a href="https://facebook.com" target="_blank"><img src="img/logofacebook.png" alt="logo facebook"></a></li>
-                <li><a href="https://instagram.com" target="_blank"><img src="img/logoinstagram.png" alt="logo instagram"></a></li>
+                <li><a href="https://twitter.com" target="_blank"><img src="img/logotwitter.webp" alt="logo twitter"></a></li>
+                <li><a href="https://facebook.com" target="_blank"><img src="img/logofacebook.webp" alt="logo facebook"></a></li>
+                <li><a href="https://instagram.com" target="_blank"><img src="img/logoinstagram.webp" alt="logo instagram"></a></li>
                 <li><a class="float-right" href="termos.php ">Termos de uso</a></li>
             </ul>
            
--- header.php
+++ header.php
@@ -4,7 +4,7 @@
             <nav class="navbar navbar-expand-lg navbar-light ">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="home.php">
-                        <img src="img/logo.png" alt="logo" class="logo">
+                        <img src="img/logo.webp" alt="logo" class="logo">
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
